588cecbd7a821ae69e5302a7ecc57b68015164f7
gem85247 <gillesmontyne@gmail.com>
2025-11-06 11:59:32 +0100

    release: menu fix - theme: 19.0.2

--- src/framework/theme/components/menu/menu.component.ts
+++ src/framework/theme/components/menu/menu.component.ts
@@ -17,9 +17,9 @@ import {
   PLATFORM_ID,
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { Router, NavigationEnd, NavigationExtras } from '@angular/router';
+import { Router, NavigationEnd, NavigationExtras, NavigationSkipped } from '@angular/router';
 import { BehaviorSubject, Subject } from 'rxjs';
-import { takeUntil, filter, map } from 'rxjs/operators';
+import { takeUntil, filter, map, debounceTime } from 'rxjs/operators';
 import { NbMenuInternalService, NbMenuItem, NbMenuBag, NbMenuService, NbMenuBadgeConfig } from './menu.service';
 import { convertToBoolProperty, NbBooleanInput } from '../helpers';
 import { NB_WINDOW } from '../../theme.options';
@@ -32,16 +32,16 @@ export enum NbToggleStates {
 }
 
 @Component({
-    selector: '[nbMenuItem]',
-    templateUrl: './menu-item.component.html',
-    animations: [
-        trigger('toggle', [
-            state(NbToggleStates.Collapsed, style({ height: '0', margin: '0' })),
-            state(NbToggleStates.Expanded, style({ height: '*' })),
-            transition(`${NbToggleStates.Collapsed} <=> ${NbToggleStates.Expanded}`, animate(300)),
-        ]),
-    ],
-    standalone: false
+  selector: '[nbMenuItem]',
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('toggle', [
+      state(NbToggleStates.Collapsed, style({ height: '0', margin: '0' })),
+      state(NbToggleStates.Expanded, style({ height: '*' })),
+      transition(`${NbToggleStates.Collapsed} <=> ${NbToggleStates.Expanded}`, animate(300)),
+    ]),
+  ],
+  standalone: false,
 })
 export class NbMenuItemComponent implements DoCheck, AfterViewInit, OnDestroy {
   @Input() menuItem = <NbMenuItem>null;
@@ -55,21 +55,21 @@ export class NbMenuItemComponent implements DoCheck, AfterViewInit, OnDestroy {
   protected destroy$ = new Subject<void>();
   toggleState: NbToggleStates;
 
-  constructor(protected menuService: NbMenuService,
-              protected directionService: NbLayoutDirectionService) {}
+  constructor(protected menuService: NbMenuService, protected directionService: NbLayoutDirectionService) {}
 
   ngDoCheck() {
     this.toggleState = this.menuItem.expanded ? NbToggleStates.Expanded : NbToggleStates.Collapsed;
   }
 
   ngAfterViewInit() {
-    this.menuService.onSubmenuToggle()
+    this.menuService
+      .onSubmenuToggle()
       .pipe(
         filter(({ item }) => item === this.menuItem),
         map(({ item }: NbMenuBag) => item.expanded),
         takeUntil(this.destroy$),
       )
-      .subscribe(isExpanded => this.toggleState = isExpanded ? NbToggleStates.Expanded : NbToggleStates.Collapsed);
+      .subscribe((isExpanded) => (this.toggleState = isExpanded ? NbToggleStates.Expanded : NbToggleStates.Collapsed));
   }
 
   ngOnDestroy() {
@@ -98,9 +98,7 @@ export class NbMenuItemComponent implements DoCheck, AfterViewInit, OnDestroy {
       return 'chevron-down-outline';
     }
 
-    return this.directionService.isLtr()
-      ? 'chevron-left-outline'
-      : 'chevron-right-outline';
+    return this.directionService.isLtr() ? 'chevron-left-outline' : 'chevron-right-outline';
   }
 }
 
@@ -208,28 +206,29 @@ export class NbMenuItemComponent implements DoCheck, AfterViewInit, OnDestroy {
  * menu-submenu-item-icon-active-hover-color:
  */
 @Component({
-    selector: 'nb-menu',
-    styleUrls: ['./menu.component.scss'],
-    template: `
+  selector: 'nb-menu',
+  styleUrls: ['./menu.component.scss'],
+  template: `
     <ul class="menu-items">
       <ng-container *ngFor="let item of items">
-        <li nbMenuItem *ngIf="!item.hidden"
-            [menuItem]="item"
-            [badge]="item.badge"
-            [class.menu-group]="item.group"
-            (hoverItem)="onHoverItem($event)"
-            (toggleSubMenu)="onToggleSubMenu($event)"
-            (selectItem)="onSelectItem($event)"
-            (itemClick)="onItemClick($event)"
-            class="menu-item">
-        </li>
+        <li
+          nbMenuItem
+          *ngIf="!item.hidden"
+          [menuItem]="item"
+          [badge]="item.badge"
+          [class.menu-group]="item.group"
+          (hoverItem)="onHoverItem($event)"
+          (toggleSubMenu)="onToggleSubMenu($event)"
+          (selectItem)="onSelectItem($event)"
+          (itemClick)="onItemClick($event)"
+          class="menu-item"
+        ></li>
       </ng-container>
     </ul>
   `,
-    standalone: false
+  standalone: false,
 })
 export class NbMenuComponent implements OnInit, AfterViewInit, OnDestroy {
-
   /**
    * Tags a menu with some ID, can be later used in the menu service
    * to determine which menu triggered the action, if multiple menus exist on the page.
@@ -261,11 +260,12 @@ export class NbMenuComponent implements OnInit, AfterViewInit, OnDestroy {
 
   protected destroy$ = new Subject<void>();
 
-  constructor(@Inject(NB_WINDOW) protected window,
-              @Inject(PLATFORM_ID) protected platformId,
-              protected menuInternalService: NbMenuInternalService,
-              protected router: Router) {
-  }
+  constructor(
+    @Inject(NB_WINDOW) protected window,
+    @Inject(PLATFORM_ID) protected platformId,
+    protected menuInternalService: NbMenuInternalService,
+    protected router: Router,
+  ) {}
 
   ngOnInit() {
     this.menuInternalService.prepareItems(this.items);
@@ -276,7 +276,7 @@ export class NbMenuComponent implements OnInit, AfterViewInit, OnDestroy {
         filter((data: { tag: string; items: NbMenuItem[] }) => this.compareTag(data.tag)),
         takeUntil(this.destroy$),
       )
-      .subscribe(data => this.onAddItem(data));
+      .subscribe((data) => this.onAddItem(data));
 
     this.menuInternalService
       .onNavigateHome()
@@ -306,7 +306,8 @@ export class NbMenuComponent implements OnInit, AfterViewInit, OnDestroy {
 
     this.router.events
       .pipe(
-        filter(event => event instanceof NavigationEnd),
+        filter((event) => event instanceof NavigationEnd || event instanceof NavigationSkipped),
+        debounceTime(100),
         takeUntil(this.destroy$),
       )
       .subscribe(() => {
